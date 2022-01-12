ca6d010da786db0bd73a5be479456c3bb76fb3c8
Scott Bax <scottbax@Scotts-MacBook-Air.local>
2022-01-12 09:47:08 -0800

    renders diff results based on score

--- lib/result.dart
+++ lib/result.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    var resultText = 'You did it!';
+
+    if (resultScore <= 8) {
+      resultText = 'You are the best!';
+    } else if (resultScore <= 12) {
+      resultText = 'You are the bestest!';
+    } else {
+      resultText = 'You are the very bestest!';
+    }
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('No more questions!'));
+    return Center(
+        child: Text(
+      resultPhrase,
+      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      textAlign: TextAlign.center,
+    ));
   }
 }
